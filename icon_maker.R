dec01a43c5d86469a6125800c89b5258af56b6ed
Njål Foldnes <njal.foldnes@bi.no>
2026-02-02 10:25:26 +0100

    df

--- icon_maker.R
+++ icon_maker.R
@@ -20,7 +20,7 @@ p <- ggplot(ll, aes(name, value, group=id, color=factor(id)))+
   xlab(NULL)+ylab(NULL)
 
 p.sticker <- sticker(
-  p, package="FREDAG DUH165", 
+  p, package="DAG 2 DUH165", 
   filename="images/icon.png",s_width = 2,
   s_height = 1.4, s_y=1, s_x=1
 )
